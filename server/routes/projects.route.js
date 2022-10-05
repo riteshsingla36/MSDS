24dd42ae6825e9e5fc263b046415a9526f09c654
deepu024 <ojashsharma024@gmail.com>
2022-10-05 19:55:33 +0530

    add cloudinary

--- server/routes/projects.route.js
+++ server/routes/projects.route.js
@@ -1,12 +1,12 @@
 const ProjectController = require("../controllers/projects.controller");
-
 const router = require("express").Router();
+const upload = require("../middleware/cloudinary");
 
 router.get("/", ProjectController.getAllProjects);
 
 router.get("/:id", ProjectController.getProjectById);
 
-router.post("/create", ProjectController.createProject);
+router.post("/create", upload.array('image', 10),ProjectController.createProject);
 
 router.patch("/update/:id", ProjectController.updateProject);
 
