4be0f7f0695b1865606b4ff4bd0ee1db5df709dd
deepu024 <ojashsharma024@gmail.com>
2022-10-05 21:17:09 +0530

    update

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config({ path: "./.env" });
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.CLOUD_API_KEY, 
-    api_secret: process.env.np_MYCSn51fttpIt1FW83wnPljc 
+    api_secret: process.env.CLOUD_API_SECRET_KEY
 });
 
 mongoose.connect(process.env.DB_URI)
