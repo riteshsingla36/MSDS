6fdc29b92c5d6e4cb1c9a4c8c9a78fe56b1dfd19
deepu024 <ojashsharma024@gmail.com>
2022-10-06 15:56:20 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -11,22 +11,25 @@ const jobsRouter = require('./routes/jobs.route');
 const projectsRouter = require('./routes/projects.route');
 const projectTypeRouter = require('./routes/projectType.route');
 
+app.use(cors());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 dotenv.config({ path: "./.env" });
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.CLOUD_API_KEY, 
-    api_secret: process.env.CLOUD_API_SECRET_KEY
+    api_secret: process.env.CLOUD_API_SECRET_KEY,
+    secure: true
 });
 
 mongoose.connect(process.env.DB_URI)
 .then(() => console.log("moongoose connected successfully"))
 .catch(error => console.log(error.meaasge));
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/careers", jobsRouter);
 app.use("/projects", projectsRouter);
